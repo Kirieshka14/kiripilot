5746af430c5a45c98c49ad425d390adf367433d9
Kirieshka14 <riz.riz1941@gmail.com>
2026-06-22 01:10:33 +0400

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ app.use(express.json());
 
 app.get('/',           (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/start',      (req, res) => res.sendFile(path.join(__dirname, 'public', 'start.html')));
+app.get('/chat',       (req, res) => res.sendFile(path.join(__dirname, 'public', 'chat.html')));
 app.get('/kiri-admin', (req, res) => res.sendFile(path.join(__dirname, 'public', 'admin.html')));
 
 function notifyAdminsUserOnline(sessionId, session) {
